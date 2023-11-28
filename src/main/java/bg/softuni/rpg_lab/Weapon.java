e9fababd2ec9fddeaa7c94d9827bb25cc928c4b3
AntoanYosifov <tapaktapxaomi@gmail.com>
2023-11-28 02:01:25 +0100

    UnitTesting

---
+++ src/main/java/bg/softuni/rpg_lab/Weapon.java
@@ -0,0 +1,7 @@
+package bg.softuni.rpg_lab;
+
+public interface Weapon {
+int getAttackPoints();
+int getDurabilityPoints();
+void attack(Target dummy);
+}
---
+++ src/test/java/bg/softuni/rpg_lab/AxeTests.java
@@ -0,0 +1,31 @@
+package bg.softuni.rpg_lab;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AxeTests {
+    @Test
+    public void testDurability() {
+        // Arrange
+        Axe axe = new Axe(10, 10);
+        Dummy target = new Dummy(10, 10);
+        // Act
+        axe.attack(target);
+
+        // Assert
+        Assert.assertEquals(9, axe.getDurabilityPoints());
+    }
+
+    // Assert
+    @Test(expected = IllegalStateException.class)
+    public void testAttackBroken(){
+        // Arrange
+        Axe axe = new Axe(10, 0);
+        Dummy target = new Dummy(10, 10);
+
+
+        // Act
+        axe.attack(target);
+
+    }
+}
